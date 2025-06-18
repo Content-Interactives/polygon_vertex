35ff14f32d16810178d5f43a56e70a7be32c2d59
TimothyCruzCk12 <timothy.cruz.intern@ck12.org>
2025-06-18 10:47:51 -0700

    fixed incorrect outline color

--- src/components/PolygonVertex.jsx
+++ src/components/PolygonVertex.jsx
@@ -247,7 +247,7 @@ const PolygonVertex = () => {
                       onChange={(e) => setUserAnswer(e.target.value)}
                       placeholder="Enter answer"
                       className={`w-40 px-3 py-2 border rounded-lg ${
-                        hasError ? 'border-red-500 focus:ring-red-500' : 'border-gray-200 focus:ring-[#008545]'
+                        hasError ? 'border-yellow-500 focus:ring-yellow-500' : 'border-gray-200 focus:ring-[#008545]'
                       } focus:outline-none focus:ring-2`}
                     />
                     <div className="glow-button simple-glow">
